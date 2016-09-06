a8211560db05b45cdd4c308d1a7322145def1f61
sebablasko <sebablasko@gmail.com>
2016-09-06 11:17:13 -0300

    Separated logic for installation and starting up of server

--- install.sh
+++ install.sh
@@ -1,15 +1,24 @@
 
 # Create vEnv
+echo "Creating virtual environment..."
 virtualenv venv_adkweb
 source venv_adkweb/bin/activate
+echo "OK"
 
 # Install requirements
+echo "Installing requirements..."
 pip install -r requirements.txt
+echo "OK"
+
+# Turn off vEnv
+echo "Shutting down virtual environment..."
+deactivate
+echo "OK"
 
 # Add external libraries
+echo "Downloading external libraries..."
 cd static
+echo "NetworkInformation-Library"
 git clone https://github.com/niclabs/NetworkInformation-Library.git
+echo "OK"
 cd ..
-
-# Start Servert
-python server.py
---
+++ run.sh
@@ -0,0 +1,8 @@
+
+# Starting vEnv
+echo "Starting virtual environment..."
+source venv_adkweb/bin/activate
+echo "OK"
+
+echo "Initializing server..."
+python server.py
