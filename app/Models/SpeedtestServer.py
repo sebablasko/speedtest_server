d7c62023d126943104f334e0f433735d0d8b8a74
sebablasko <sebablasko@gmail.com>
2016-09-14 12:23:21 -0300

    changed the model of speedtestservers

--- app/Models/SpeedtestServer.py
+++ app/Models/SpeedtestServer.py
@@ -1,19 +1,15 @@
 from app.dbmanager import db
 
 class SpeedtestServer(db.Model):
-    ip_address = db.Column(db.String(15), primary_key=True)
-    port_address = db.Column(db.Integer, primary_key=True)
-    url = db.Column(db.String(100), primary_key=True)
+    url = db.Column(db.String(120), primary_key=True)
     added = db.Column(db.DateTime, server_default=db.func.current_timestamp())
 
-    def __init__(self, ip,port,url):
+    def __init__(self, url):
         super(SpeedtestServer, self).__init__()
-        self.ip_address = ip
-        self.port_address = port
         self.url = url
 
     def __repr__(self):
-        return '<%s (%s:%s)>' % (self.url,self.ip_address,self.port_address)
+        return '<%s>' % (self.url)
 
     def as_dict(self):
         return dict( (c.name, getattr(self, c.name))
